8cff95d246f81da62afc1569fb708d59d536479d
malcom <malcompozo@gmail.com>
2021-09-30 20:29:44 -0300

    configurando modulo de paginado, configurado manual de lazyLoad de PostsComponent, implementacion de servicios y data externa API, consumir y mostrar en el componente

---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  mensajes: any;
+
+  constructor( private data: DataService) { }
+
+  ngOnInit() {
+
+    this.mensajes = this.data.getPost()
+      /* .subscribe( (posts: any ) => {
+        console.log(posts); 
+        this.mensajes = posts;} ); */
+
+  }
+
+}
